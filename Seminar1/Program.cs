62012463ab5412d2aee502a33c2de65c328e2e79
VasiliPuchov <89031200106@mail.ru>
2022-10-13 18:35:19 +0300

    home1.1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max;
+if(number1 > number2){
+    max = number1
+}
+else{
+    max = number2
+}
